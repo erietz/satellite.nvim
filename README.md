a5782cd59941b68d039a6b641dee161adb2e973c
Ethan Rietz <ewrietz@gmail.com>
2021-11-08 16:45:55 -0600

    obsolete

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+# OBSOLETE !!!!
+
+I just found [rest.nvim](https://github.com/NTBBloodbath/rest.nvim) which
+already did everything that I wanted.
+
 Primitive example: ![Demo](./media/Peek-2021-11-06-18-10.gif)
 
 Inspiration: [Satellite of Love](https://www.youtube.com/watch?v=KFbgAxxVh1E)
--- lua/satellite/job.lua
+++ lua/satellite/job.lua
@@ -1,24 +1,41 @@
+local Request = require('satellite.request')
+local luajob = require('luajob')
+local ls = luajob:new({
+    cmd = 'ls -al',
+    on_stdout = function(err, data)
+        print(data)
+    end
+    })
+
+
 local M = {}
 
-local function on_event(job_id, data, event)
-    if event == 'stdout' then
-        print(vim.inspect(data))
-    elseif event == 'stderr' then
-        print(vim.inspect(data))
-    else
-    end
-end
+-- local function on_event(job_id, data, event)
+--     if event == 'stdout' then
+--         print(vim.inspect(data))
+--         print(vim.api.nvim_get_vvar('self.stdout_queue'))
+--     elseif event == 'stderr' then
+--         print(vim.inspect(data))
+--     else
+--         -- local r = Request:new()
+--         -- r.parse_response()
+--         -- r.write_body_to_buffer()
+--     end
+-- end
 
-local function run_job(cmd)
-    local callbacks = {
-        on_stdout = on_event,
-        on_stderr = on_event,
-        on_exit = on_event
-    }
-    vim.fn.jobstart(cmd, callbacks)
-end
+-- local function run_job(cmd)
+--     local callbacks = {
+--         stdout_queue = {'test'},
+--         stderr_queue = {'test'},
+--         on_stdout = on_event,
+--         on_stderr = on_event,
+--         on_exit = on_event
+--     }
+--     vim.fn.jobstart(cmd, callbacks)
+-- end
 
 
-M.run_job = run_job
+-- M.run_job = run_job
+M.run_job = ls.start
 
 return M
